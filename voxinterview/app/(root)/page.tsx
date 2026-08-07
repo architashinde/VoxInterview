de53c9fbf7aaa748ef9e28d6d0386d89449632b7
architashinde <architashinde45@gmail.com>
2026-08-08 00:52:23 +0530

    updated favicon

---
+++ voxinterview/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-2xl">Home Page</h1>
+    </div>
+  );
+}
+
+export default Page;
